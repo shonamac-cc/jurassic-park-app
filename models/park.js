472f9944c0f067b08518e276b40243ccadf06468
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-09-03 16:50:21 +0100

    added tests for park name and ticket price

---
+++ models/park.js
@@ -0,0 +1,9 @@
+const Park = function (name, ticketPrice) {
+  this.name = name;
+  this.ticketPrice = ticketPrice;
+}
+
+
+
+
+module.exports = Park
---
+++ specs/park_spec.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  let park;
+
+  beforeEach(function () {
+    park = new Park('Jurassic', 20);
+  });
+
+  it('should have a name', function () {
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurassic');
+  });
+
+  it('should have a ticket price', function () {
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 20);
+  });
+
+  it('should have a collection of dinosaurs');
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to calculate the total number of visitors per day');
+
+  it('should be able to calculate the total number of visitors per year');
+
+  it('should be able to calculate total revenue for one year');
+
+});
